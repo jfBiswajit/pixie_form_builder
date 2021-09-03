e8083d93b07bd468ecf6e0cc2c12c7e7a1ee7448
Biswajit Biswas <jfbiswajit@gmail.com>
2021-09-03 16:14:48 +0600

    Add: create class attribute

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,3 +3,11 @@
 @isset($data['ids'])
     id={{ $data['ids'][0] }}
 @endisset
+
+{{-- if the user provides a class key then create a class attribute and show all classes --}}
+@isset($data['classes'])
+    class=
+    "@foreach ($data['classes'] as $class)
+    {{ $class }}
+    @endforeach"
+@endisset>
